84be0124440ba894162fb6b987e1a3b9ed11fb40
Vlad P <wilamkopa393@gmail.com>
2025-10-29 08:41:22 +0200

    Switch to telebot for message handling
    Refactor bot to use telebot library and update message handling.

--- laba7/bot.py
+++ laba7/bot.py
@@ -1,31 +1,28 @@
-# bot.py
 import os
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import telebot
 
-# Завантажуємо токен із .env
-load_dotenv()
+
+load_dotenv(r"C:\Users\user\PycharmProjects\PythonProject5\.env")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN не знайдено у файлі .env")
 
-# Обробник повідомлень — просто повторює текст користувача
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message:
-        await update.message.reply_text(update.message.text)
 
-async def main():
-    # Створюємо застосунок (бот)
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+bot = telebot.TeleBot(BOT_TOKEN)
 
-    # Додаємо обробник усіх повідомлень
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+def main():
     print("🤖 Бот запущений. Натисни Ctrl+C щоб зупинити.")
-    await app.run_polling()
+    bot.infinity_polling()
+
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
